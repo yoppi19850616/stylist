042492dc5a4f3d8242b64b85995618bbc547612a
yoppi19850616 <azm.y_t_m@icloud.com>
2021-12-05 23:11:08 +0900

    ユーザー登録機能編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :last_name, :first_name, :last_name_furigana, :first_name_furigana,
+                                      keys: [:email,:nickname, :last_name, :first_name, :last_name_furigana, :first_name_furigana,
                                              :phonenumber])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname,            presence: true
+  validates :last_name,           presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name_furigana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :phonenumber,         presence: true
+  validates :password,            format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
+
 end
